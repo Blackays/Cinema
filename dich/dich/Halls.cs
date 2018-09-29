a40967e77a4e5e13cca68a6dbe5096933d177993
Blackays <mni01051999@gmail.com>
2018-09-29 22:45:55 +0300

    nyyy

---
+++ dich/dich/Halls.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dich
+{
+    class Halls:Cinema
+    {
+        public int NumberOfHall { get; set; }
+        public bool TypeOfHall { get; set; }
+        public int Rows { get; set; }
+        public int Cols { get; set; }
+
+    }
+}
